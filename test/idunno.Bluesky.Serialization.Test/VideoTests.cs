61062cab8d53e89fb8d5af5655fa5dbc5ea67425
Barry Dorrans <github.com@idunno.org>
2025-04-03 02:58:27 -0700

    Fix up video status tests

--- test/idunno.Bluesky.Serialization.Test/VideoTests.cs
+++ test/idunno.Bluesky.Serialization.Test/VideoTests.cs
@@ -1,7 +1,6 @@
 ﻿// Copyright (c) Barry Dorrans. All rights reserved.
 // Licensed under the MIT License.
 
-using System.Diagnostics.CodeAnalysis;
 using System.Text.Json;
 
 using idunno.AtProto;
@@ -16,60 +15,164 @@ namespace idunno.Bluesky.Serialization.Test
         private readonly JsonSerializerOptions _jsonSerializerOptions = new(JsonSerializerDefaults.Web);
 
         [Fact]
-        public void JobStatusDeserializesCreatedJson()
+        public void JobStatusWireFormatDeserializesCreatedJson()
         {
-            string jsonString = File.ReadAllText(Path.Combine("json", "video", "jobStatus_Created.json"));
+            string jsonString = """
+            {
+                "did":"did:plc:ec72yg6n2sydzjvtovvdlxrk",
+                "jobId":"cuog2ca0ours72rbnvgg",
+                "state":"JOB_STATE_CREATED"
+            }
+            """;
 
-            JobStatus? jobStatus = JsonSerializer.Deserialize<JobStatus>(jsonString, _jsonSerializerOptions);
+            JobStatusWireFormat? jobStatusWireFormat = JsonSerializer.Deserialize<JobStatusWireFormat>(jsonString, _jsonSerializerOptions);
 
-            Assert.NotNull(jobStatus);
+            Assert.NotNull(jobStatusWireFormat);
+            Assert.Equal("JOB_STATE_CREATED", jobStatusWireFormat.StateAsString);
+            Assert.Equal("did:plc:ec72yg6n2sydzjvtovvdlxrk", jobStatusWireFormat.Did);
+            Assert.Equal("cuog2ca0ours72rbnvgg", jobStatusWireFormat.JobId);
+
+            var jobStatus = new JobStatus(jobStatusWireFormat);
+
             Assert.Equal(JobState.Created, jobStatus.State);
-            Assert.Equal(new Did("did:plc:ec72yg6n2sydzjvtovvdlxrk"), jobStatus.Did);
-            Assert.Equal("cuog2ca0ours72rbnvgg", jobStatus.JobId);
         }
 
         [Fact]
-        public void JobStatusResponseCreatedJson()
+        public void JobStatusResponseDeserializesCreatedJson()
         {
-            string jsonString = File.ReadAllText(Path.Combine("json", "video", "getJobStatus_Created.json"));
+            string jsonString = """
+            {
+                "jobStatus": {
+                    "did": "did:plc:ec72yg6n2sydzjvtovvdlxrk",
+                    "jobId": "cuog2ca0ours72rbnvgg",
+                    "state": "JOB_STATE_CREATED"
+                }
+            }
+            """;
 
             JobStatusResponse? jobStatusResponse = JsonSerializer.Deserialize<JobStatusResponse>(jsonString, _jsonSerializerOptions);
 
             Assert.NotNull(jobStatusResponse);
-            Assert.Equal(JobState.Created, jobStatusResponse.JobStatus.State);
-            Assert.Equal(new Did("did:plc:ec72yg6n2sydzjvtovvdlxrk"), jobStatusResponse.JobStatus.Did);
+            Assert.Equal("JOB_STATE_CREATED", jobStatusResponse.JobStatus.StateAsString);
+            Assert.Equal("did:plc:ec72yg6n2sydzjvtovvdlxrk", jobStatusResponse.JobStatus.Did);
             Assert.Equal("cuog2ca0ours72rbnvgg", jobStatusResponse.JobStatus.JobId);
+
+            var jobStatus = new JobStatus(jobStatusResponse.JobStatus);
+
+            Assert.Equal(JobState.Created, jobStatus.State);
         }
 
         [Fact]
-        public void JobStatusDeserializesAlreadyExistsJson()
+        public void JobStatusWireFormatDeserializesAlreadyExistsJson()
         {
-            string jsonString = File.ReadAllText(Path.Combine("json", "video", "jobStatus_AlreadyExists.json"));
+            string jsonString = """
+            {
+                "did": "did:plc:ec72yg6n2sydzjvtovvdlxrk",
+                "error": "already_exists",
+                "jobId": "cvcaag996ogs72sgc1p0",
+                "message": "Video already processed",
+                "state": "JOB_STATE_COMPLETED"
+            }
+            """;
 
-            JobStatus? jobStatus = JsonSerializer.Deserialize<JobStatus>(jsonString, _jsonSerializerOptions);
+            JobStatusWireFormat? jobStatusWireFormat = JsonSerializer.Deserialize<JobStatusWireFormat>(jsonString, _jsonSerializerOptions);
 
-            Assert.NotNull(jobStatus);
+            Assert.NotNull(jobStatusWireFormat);
+            Assert.Equal("JOB_STATE_COMPLETED", jobStatusWireFormat.StateAsString);
+            Assert.Equal("did:plc:ec72yg6n2sydzjvtovvdlxrk", jobStatusWireFormat.Did);
+            Assert.Equal("cvcaag996ogs72sgc1p0", jobStatusWireFormat.JobId);
+            Assert.Equal("already_exists", jobStatusWireFormat.Error);
+            Assert.Equal("Video already processed", jobStatusWireFormat.Message);
+
+            var jobStatus = new JobStatus(jobStatusWireFormat);
+
             Assert.Equal(JobState.Completed, jobStatus.State);
-            Assert.Equal(new Did("did:plc:ec72yg6n2sydzjvtovvdlxrk"), jobStatus.Did);
-            Assert.Equal("cvcaag996ogs72sgc1p0", jobStatus.JobId);
-            Assert.Equal("already_exists", jobStatus.Error);
-            Assert.Equal("Video already processed", jobStatus.Message);
         }
 
         [Fact]
         public void JobStatusResponseDeserializesAlreadyExistsJson()
         {
-            string jsonString = File.ReadAllText(Path.Combine("json", "video", "getJobStatus_AlreadyExists.json"));
+            string jsonString = """
+            {
+                "jobStatus": {
+                    "did": "did:plc:ec72yg6n2sydzjvtovvdlxrk",
+                    "error": "already_exists",
+                    "jobId": "cvcaag996ogs72sgc1p0",
+                    "message": "Video already processed",
+                    "state": "JOB_STATE_COMPLETED"
+                }
+            }
+            """;
 
             JobStatusResponse? jobStatusResponse = JsonSerializer.Deserialize<JobStatusResponse>(jsonString, _jsonSerializerOptions);
 
             Assert.NotNull(jobStatusResponse);
-            Assert.Equal(JobState.Completed, jobStatusResponse.JobStatus.State);
+            Assert.Equal("JOB_STATE_COMPLETED", jobStatusResponse.JobStatus.StateAsString);
             Assert.Equal(new Did("did:plc:ec72yg6n2sydzjvtovvdlxrk"), jobStatusResponse.JobStatus.Did);
             Assert.Equal("cvcaag996ogs72sgc1p0", jobStatusResponse.JobStatus.JobId);
             Assert.Equal("already_exists", jobStatusResponse.JobStatus.Error);
             Assert.Equal("Video already processed", jobStatusResponse.JobStatus.Message);
+
+            var jobStatus = new JobStatus(jobStatusResponse.JobStatus);
+
+            Assert.Equal(JobState.Completed, jobStatus.State);
         }
 
+        [Fact]
+        public void JobStatusResponseDeserializesFailedJson()
+        {
+            string jsonString = """
+            {
+                "jobStatus": {
+                    "did": "did:plc:ec72yg6n2sydzjvtovvdlxrk",
+                    "error": "failed",
+                    "jobId": "cvcaag996ogs72sgc1p0",
+                    "message": "Video processing failed",
+                    "state": "JOB_STATE_FAILED"
+                }
+            }
+            """;
+
+            JobStatusResponse? jobStatusResponse = JsonSerializer.Deserialize<JobStatusResponse>(jsonString, _jsonSerializerOptions);
+
+            Assert.NotNull(jobStatusResponse);
+            Assert.Equal("JOB_STATE_FAILED", jobStatusResponse.JobStatus.StateAsString);
+            Assert.Equal(new Did("did:plc:ec72yg6n2sydzjvtovvdlxrk"), jobStatusResponse.JobStatus.Did);
+            Assert.Equal("cvcaag996ogs72sgc1p0", jobStatusResponse.JobStatus.JobId);
+            Assert.Equal("failed", jobStatusResponse.JobStatus.Error);
+            Assert.Equal("Video processing failed", jobStatusResponse.JobStatus.Message);
+
+            var jobStatus = new JobStatus(jobStatusResponse.JobStatus);
+
+            Assert.Equal(JobState.Failed, jobStatus.State);
+        }
+
+        [Fact]
+        public void JobStatusResponseDefaultsToInProgressWhenDeserializingUnkdocumentaedStatusJson()
+        {
+            string jsonString = """
+            {
+                "jobStatus": {
+                    "did": "did:plc:ec72yg6n2sydzjvtovvdlxrk",
+                    "jobId": "cvcaag996ogs72sgc1p0",
+                    "message": "Video processing doing something",
+                    "state": "JOB_STATE_UNDOCUMENTED"
+                }
+            }
+            """;
+
+            JobStatusResponse? jobStatusResponse = JsonSerializer.Deserialize<JobStatusResponse>(jsonString, _jsonSerializerOptions);
+
+            Assert.NotNull(jobStatusResponse);
+            Assert.Equal("JOB_STATE_UNDOCUMENTED", jobStatusResponse.JobStatus.StateAsString);
+            Assert.Equal(new Did("did:plc:ec72yg6n2sydzjvtovvdlxrk"), jobStatusResponse.JobStatus.Did);
+            Assert.Equal("cvcaag996ogs72sgc1p0", jobStatusResponse.JobStatus.JobId);
+            Assert.Null(jobStatusResponse.JobStatus.Error);
+            Assert.Equal("Video processing doing something", jobStatusResponse.JobStatus.Message);
+
+            var jobStatus = new JobStatus(jobStatusResponse.JobStatus);
+
+            Assert.Equal(JobState.InProgress, jobStatus.State);
+        }
     }
 }
