601ff37cd1718dbb1050aeded4f4378676b87116
Barry Dorrans <github.com@idunno.org>
2025-10-31 16:43:13 -0700

    Add optional via property to follow record.

--- src/idunno.Bluesky/Record/Follow.cs
+++ src/idunno.Bluesky/Record/Follow.cs
@@ -3,6 +3,7 @@
 
 using System.Text.Json.Serialization;
 using idunno.AtProto;
+using idunno.AtProto.Repo;
 
 namespace idunno.Bluesky.Record
 {
@@ -15,8 +16,10 @@ namespace idunno.Bluesky.Record
         /// Creates a new instance of <see cref="Follow"/> with <see cref="BlueskyTimestampedRecord.CreatedAt"/> set to the current date and time.
         /// </summary>
         /// <param name="subject">The <see cref="Did"/> to the actor to be followed.</param>
-        public Follow(Did subject) : this(subject, DateTimeOffset.UtcNow)
+        /// <param name="via">A <see cref="StrongReference"/> to the starter pack the follow originated from, if any.</param>
+        public Follow(Did subject, StrongReference? via = null) : this(subject: subject, via: via, createdAt: DateTimeOffset.UtcNow)
         {
+            ArgumentNullException.ThrowIfNull(subject);
         }
 
         /// <summary>
@@ -25,10 +28,23 @@ namespace idunno.Bluesky.Record
         /// <param name="subject">The <see cref="Did"/> to the actor to be followed.</param>
         /// <param name="createdAt">The <see cref="DateTimeOffset"/> for the repost creation date.</param>
         [JsonConstructor]
-        public Follow(Did subject, DateTimeOffset createdAt) : base(createdAt)
+        public Follow(Did subject, DateTimeOffset createdAt) : this(subject: subject, via: null, createdAt: createdAt)
+        {
+            ArgumentNullException.ThrowIfNull(subject);
+        }
+
+        /// <summary>
+        /// Creates a new instance of <see cref="Follow"/>.
+        /// </summary>
+        /// <param name="subject">The <see cref="Did"/> to the actor to be followed.</param>
+        /// <param name="via">A <see cref="StrongReference"/> to the starter pack the follow originated from, if any.</param>
+        /// <param name="createdAt">The <see cref="DateTimeOffset"/> for the repost creation date.</param>
+        [JsonConstructor]
+        public Follow(Did subject, StrongReference? via, DateTimeOffset createdAt) : base(createdAt)
         {
             ArgumentNullException.ThrowIfNull(subject);
             Subject = subject;
+            Via = via;
         }
 
         /// <summary>
@@ -36,5 +52,10 @@ namespace idunno.Bluesky.Record
         /// </summary>
         [JsonInclude]
         public Did Subject { get; init; }
+
+        /// <summary>
+        /// Gets a <see cref="StrongReference"/> to the starter pack the follow originated from.
+        /// </summary>
+        public StrongReference? Via { get; init; }
     }
 }
