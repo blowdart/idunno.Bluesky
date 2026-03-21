6ea2a8c4b87b78363e50cb3099ead754af919a01
Barry Dorrans <github.com@idunno.org>
2026-03-21 12:27:12 -0700

    Remove unneeded namespace

--- src/idunno.AtProto/Jetstream/AtProtoJetStream.cs
+++ src/idunno.AtProto/Jetstream/AtProtoJetStream.cs
@@ -2,7 +2,6 @@
 // Licensed under the MIT License.
 
 using System.Diagnostics.CodeAnalysis;
-using System.Diagnostics.Metrics;
 using System.Globalization;
 using System.Net.WebSockets;
 using System.Text;
