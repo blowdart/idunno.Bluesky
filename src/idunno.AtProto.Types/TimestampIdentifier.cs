7f5c580788e769a9336a4ce585f584fe55fbbea1
Barry Dorrans <github.com@idunno.org>
2026-06-27 17:23:18 -0700

    Remove unneeded namespace Remove BOMs

--- src/idunno.AtProto.Types/TimestampIdentifier.cs
+++ src/idunno.AtProto.Types/TimestampIdentifier.cs
@@ -4,7 +4,6 @@
 using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 using System.Runtime.CompilerServices;
-using System.Security.Cryptography;
 using System.Text.Json.Serialization;
 using System.Text.RegularExpressions;
 
